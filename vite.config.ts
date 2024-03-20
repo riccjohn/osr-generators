dc26573ab255d38129b5a5e14094d4ad968d07bd
John Riccardi <riccjohn@gmail.com>
2024-03-20 16:36:22 -0500

    Set up eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] } },
   resolve: { alias: { src: resolve('src/') } },
+  plugins: [eslint()],
   test: {
     setupFiles: ['./setupTests.ts'],
     coverage: {
