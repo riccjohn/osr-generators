da00aa924e2828ef3a266dd593a2f5f8d50953fa
John Riccardi <riccjohn@gmail.com>
2024-03-20 16:16:36 -0500

    Set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
@@ -5,4 +6,18 @@ import { resolve } from 'path'
 export default defineConfig({
   build: { lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] } },
   resolve: { alias: { src: resolve('src/') } },
+  test: {
+    setupFiles: ['./setupTests.ts'],
+    coverage: {
+      thresholds: {
+        lines: 95,
+        branches: 95,
+        functions: 95,
+        statements: 95,
+      },
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{ts}'],
+    }
+  }
 })
