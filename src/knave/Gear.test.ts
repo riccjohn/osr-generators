84abe0b45f185ab9928a1ff2532ed2af5d202748
John Riccardi <riccjohn@gmail.com>
2024-06-10 20:54:54 -0500

    Refactor Gear class

--- src/knave/Gear.test.ts
+++ src/knave/Gear.test.ts
@@ -1,46 +1,47 @@
+import { vi } from 'vitest'
 import armorList from './data/armor.json'
 import gearList from './data/gear.json'
 import weaponList from './data/weapons.json'
-import Gear from './Gear'
+import Gear, { type GearItem } from './Gear'
 
 vi.mock('../dice/Randomization')
 
 describe('Gear', () => {
   const itemSlots = 12
-  let gear: Gear
-
-  beforeEach(() => {
-    gear = new Gear(itemSlots)
-  })
 
   describe('initialization', () => {
     test('sets the max item slots', () => {
+      const gear = new Gear(itemSlots)
       expect(gear.itemSlots).toEqual(itemSlots)
     })
   })
 
   describe('items', () => {
     test('contains 2 days of rations', () => {
-      const rations = gear.items.filter((g: IGear) => g.name === 'rations')
+      const gear = new Gear(itemSlots)
+      const rations = gear.items.filter(gear => gear.name === 'rations')
       expect(rations).toHaveLength(2)
     })
 
     test('contains 2 pieces of dungeonnering gear', () => {
-      const dungeoneeringGear = gear.items.filter((item: IGear) =>
+      const gear = new Gear(itemSlots)
+      const dungeoneeringGear = gear.items.filter(item =>
         gearList.dungeoneeringGear.includes(item),
       )
       expect(dungeoneeringGear).toHaveLength(2)
     })
 
     test('has one piece of gear from General Gear Set 1', () => {
-      const generalGear1 = gear.items.filter((item: IGear) =>
+      const gear = new Gear(itemSlots)
+      const generalGear1 = gear.items.filter(item =>
         gearList.generalGearSetOne.includes(item),
       )
       expect(generalGear1).toHaveLength(1)
     })
 
     test('has one piece of gear from General Gear 2', () => {
-      const generalGear2 = gear.items.filter((item: IGear) =>
+      const gear = new Gear(itemSlots)
+      const generalGear2 = gear.items.filter(item =>
         gearList.generalGearSetTwo.includes(item),
       )
       expect(generalGear2).toHaveLength(1)
@@ -49,11 +50,13 @@ describe('Gear', () => {
 
   describe('armor', () => {
     test('selects a random piece of armor', () => {
+      const gear = new Gear(itemSlots)
       expect(armorList.armor.includes(gear.armor)).toBeTruthy()
     })
 
     test('adds the armor to the characters gear', () => {
-      const armor = gear.items.filter((item: IGear) => item.type === 'armor')
+      const gear = new Gear(itemSlots)
+      const armor = gear.items.filter(item => item.type === 'armor')
 
       expect(armor).toHaveLength(1)
     })
@@ -61,11 +64,15 @@ describe('Gear', () => {
 
   describe('weapon', () => {
     test('selects a random weapon', () => {
+      const gear = new Gear(itemSlots)
       expect(weaponList.weapons.includes(gear.weapon)).toBeTruthy()
     })
 
     test('adds the weapon to the characters gear', () => {
-      const weapon = gear.items.filter((item: IGear) => item.type === 'weapon')
+      const gear = new Gear(itemSlots)
+      const weapon = gear.items.filter(
+        (item: GearItem) => item.type === 'weapon',
+      )
       expect(weapon).toHaveLength(1)
     })
   })
--- src/knave/Gear.ts
+++ src/knave/Gear.ts
@@ -3,72 +3,96 @@ import gearList from './data/gear.json'
 import weaponList from './data/weapons.json'
 import { Randomization } from '../dice'
 
+type GearType = 'food' | 'tool' | 'light' | 'armor' | 'weapon'
+
+export type GearItem = {
+  count: number
+  name: string
+  slots: number
+  type: GearType
+}
+
+type ArmorItem = GearItem & {
+  quality: number
+  defense: number
+}
+
+type WeaponItem = GearItem & {
+  damage: string
+  hand: number
+  quality: number
+}
+
 class Gear {
   public itemSlots: number
-  public items: IGear[]
-  public armor: IArmor
-  public weapon: IWeapon
+  public items: GearItem[]
+  public armor: ArmorItem
+  public weapon: WeaponItem
   private itemSlotsUsed: number
 
   constructor(itemSlots: number) {
-    // Order matters! this.getRandomGear must be last
     this.itemSlots = itemSlots
-    this.itemSlotsUsed = 6
+    this.itemSlotsUsed = 0
+    this.items = this.getRandomGear()
     this.armor = this.getRandomArmor()
     this.weapon = this.getRandomWeapon()
-    this.items = this.getRandomGear()
   }
 
-  private getRandomArmor = (): IArmor => {
+  private getRandomArmor = (): ArmorItem => {
     const slotsRemaining = this.itemSlots - this.itemSlotsUsed
 
-    const availableArmor = (armorList.armor as IArmor[]).filter(armor => {
+    const availableArmor = (armorList.armor as ArmorItem[]).filter(armor => {
       // Ensures there is at least 1 slot remaining for a weapon to be added
       return armor.slots < slotsRemaining
     })
     const armor = Randomization.getRandomItem(availableArmor)
+
     this.itemSlotsUsed += armor.slots
+    this.items.push(armor)
     return armor
   }
 
-  private getRandomWeapon = (): IWeapon => {
-    const slotsRemaining = this.itemSlots - this.itemSlotsUsed
-    const availableWeapons = (weaponList.weapons as IWeapon[]).filter(
-      weapon => {
-        return weapon.slots <= slotsRemaining
-      },
-    )
-    const weapon = Randomization.getRandomItem(availableWeapons)
-    this.itemSlotsUsed += weapon.slots
-    return weapon
-  }
+  private getRandomGear(): GearItem[] {
+    const dg = gearList.dungeoneeringGear as GearItem[]
+    const gen1 = gearList.generalGearSetOne as GearItem[]
+    const gen2 = gearList.generalGearSetTwo as GearItem[]
 
-  private getRandomGear(): IGear[] {
-    const dg = gearList.dungeoneeringGear as IGear[]
-    const gen1 = gearList.generalGearSetOne as IGear[]
-    const gen2 = gearList.generalGearSetTwo as IGear[]
-
-    const startingGear: IGear[] = [
+    const startingGear: GearItem[] = [
       { name: 'rations', count: 1, slots: 1, type: 'food' },
       { name: 'rations', count: 1, slots: 1, type: 'food' },
     ]
 
-    const dungeoneeringGear: IGear[] = new Array(2)
+    const dungeoneeringGear: GearItem[] = new Array(2)
       .fill(undefined)
       .map(() => Randomization.getRandomItem(dg))
 
     const generalGear1 = Randomization.getRandomItem(gen1)
     const generalGear2 = Randomization.getRandomItem(gen2)
 
-    // TODO: refactor so this isn't dependent on getRandomArmor being called first in the constructor
-    return [
+    const gear = [
       ...startingGear,
       ...dungeoneeringGear,
       generalGear1,
       generalGear2,
-      this.armor,
-      this.weapon,
     ]
+
+    const itemSlotsUsed = gear.reduce((acc, curr) => (acc += curr.slots), 0)
+    this.itemSlotsUsed = itemSlotsUsed
+
+    return gear
+  }
+
+  private getRandomWeapon = (): WeaponItem => {
+    const slotsRemaining = this.itemSlots - this.itemSlotsUsed
+    const availableWeapons = (weaponList.weapons as WeaponItem[]).filter(
+      weapon => {
+        return weapon.slots <= slotsRemaining
+      },
+    )
+    const weapon = Randomization.getRandomItem(availableWeapons)
+    this.itemSlotsUsed += weapon.slots
+    this.items.push(weapon)
+    return weapon
   }
 }
 
